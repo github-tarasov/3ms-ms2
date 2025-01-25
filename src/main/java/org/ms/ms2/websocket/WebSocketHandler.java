80d9c3b6762345ee95c0cf5d7856a19ba417c044
Anton Tarasov <ATarasov@bi-telco.com>
2025-01-25 21:22:35 +0300

    Fix Jaeger config

--- src/main/java/org/ms/ms2/websocket/WebSocketHandler.java
+++ src/main/java/org/ms/ms2/websocket/WebSocketHandler.java
@@ -2,24 +2,14 @@ package org.ms.ms2.websocket;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
-import io.opentelemetry.context.propagation.ContextPropagators;
-import io.opentelemetry.exporter.logging.LoggingSpanExporter;
-import io.opentelemetry.instrumentation.annotations.SpanAttribute;
-import io.opentelemetry.instrumentation.annotations.WithSpan;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ms.dto.Message;
 import org.ms.ms2.service.MS2Service;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -34,8 +24,9 @@ public class WebSocketHandler extends TextWebSocketHandler {
     private Tracer tracer;
 
     @Override
-    @Transactional
-    @WithSpan("Handle Message from MS2 websocket")
+    /*
+     *  Handle Message from MS2 websocket
+     */
     protected void handleTextMessage(WebSocketSession session, TextMessage textMessage) throws JsonProcessingException {
         Span span = tracer.spanBuilder("WebSocket handler")
                         .setAttribute(AttributeKey.stringKey("message"), textMessage.getPayload())
